422f2757b466f52dbe5953b36c4a165fc28be876
lenguyen17 <lenguyencm97@gmail.com>
2023-11-03 02:57:40 +0700

    admin page

--- View/admin/suasanpham.php
+++ View/admin/suasanpham.php
@@ -8,48 +8,50 @@ if (isset($_GET['id_ctlaptop'])) {
 ?>
 
 <div class="container w-75">
-    <h4 class="text-center fw-bold">SỬA SẢN PHẨM</h4>
+    <h4 class="text-center fw-bold my-3">SỬA SẢN PHẨM</h4>
     <form action="" method="post">
-        <div class="form-group">
-            <label for="">Mã sản phẩm</label>
-            <input type="number" class="form-control" value="<?php echo $item['id_ctlaptop']; ?>">
+        <div class="row">
+            <div class="form-group col-lg-6 mb-2">
+                <label for="">Mã sản phẩm</label>
+                <input type="number" class="form-control" value="<?php echo $item['id_ctlaptop']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Tên sản phẩm</label>
+                <input type="text" class="form-control" value="<?php echo $item['ten_laptop']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Loại</label>
+                <input type="text" class="form-control" value="<?php echo $item['ten_loai']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Cấu hình</label>
+                <input type="text" class="form-control" value="<?php echo $item['dung_luong']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Màn hình</label>
+                <input type="text" class="form-control" value="<?php echo $item['man_hinh']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Màu</label>
+                <input type="text" class="form-control" value="<?php echo $item['ten_mau']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Mô tả</label>
+                <textarea name="mieu_ta" id="" class="form-control" cols="30" rows="10"><?php echo $item['mieu_ta']; ?></textarea>
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Lượt xem</label>
+                <input type="number" class="form-control" value="<?php echo $item['so_luot_xem']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Khuyến mãi (%)</label>
+                <input type="number" class="form-control" value="<?php echo $item['giam_gia']; ?>">
+            </div>
+            <div class="form-group col-lg-6 my-2">
+                <label for="">Số lượng</label>
+                <input type="number" class="form-control" value="<?php echo $item['so_luong']; ?>">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="">Tên sản phẩm</label>
-            <input type="text" class="form-control" value="<?php echo $item['ten_laptop']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Loại</label>
-            <input type="text" class="form-control" value="<?php echo $item['ten_loai']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Cấu hình</label>
-            <input type="text" class="form-control" value="<?php echo $item['dung_luong']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Màn hình</label>
-            <input type="text" class="form-control" value="<?php echo $item['man_hinh']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Màu</label>
-            <input type="text" class="form-control" value="<?php echo $item['ten_mau']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Mô tả</label>
-            <input type="text" class="form-control" value="<?php echo $item['mieu_ta']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Lượt xem</label>
-            <input type="number" class="form-control" value="<?php echo $item['so_luot_xem']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Khuyến mãi (%)</label>
-            <input type="number" class="form-control" value="<?php echo $item['giam_gia']; ?>">
-        </div>
-        <div class="form-group">
-            <label for="">Số lượng</label>
-            <input type="number" class="form-control" value="<?php echo $item['so_luong']; ?>">
-        </div>
-
+        <button class="btn btn-primary float-end">Cập nhật</button>
     </form>
 </div>
